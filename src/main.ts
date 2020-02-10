67f65425b62d91d65d43275f7153c7843d980b1a
raymond.sutrisno <raymond.sutrisno@slalom.com>
2020-02-10 14:51:34 -0600

    called enableProdMode()

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './polyfills';
 
 import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
